6f9eb9017b8b702c110d9e7e0bc6e733691dde4d
DenisFovas <fovasdenis@gmail.com>
2017-09-16 18:03:41 +0000

    Replaces he TyniCME with a personal WYSIWYG editor. Repaced the textarea with an iframe.

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,9 +1,26 @@
-tinymce.init({ 
-    selector: 'textarea#text-editor',
-    toolbar: "styleselect | bold italic underline | alignleft aligncenter alignright alignjusify | indent outdent | blockquote | undo redo",
-    menubar: false,
-    theme: "modern",
-    // This is the css file for the text editor.
-    content_css: "/css/editor.css",
-    allow_unsafe_link_targer: true
+// tinymce.init({ 
+//     selector: 'textarea#text-editor',
+//     toolbar: "styleselect | bold italic underline | alignleft aligncenter alignright alignjusify | indent outdent | blockquote | undo redo",
+//     menubar: false,
+//     theme: "modern",
+//     // This is the css file for the text editor.
+//     content_css: "/css/editor.css",
+//     allow_unsafe_link_targer: true
+// });
+
+$(document).ready( function() {
+    // Will enable the design mode for the iframe
+    editor.document.designMode = "On";
+});
+
+function executeCommand (command) {
+    editor.document.execCommand(command, false, null);
+};
+
+function executeCommandWithArguments(command, argument) {
+    editor.document.execCommand(command, false, argument);
+}
+
+$("#submit-button").on('click', function() {
+
 });
