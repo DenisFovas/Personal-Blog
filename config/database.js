2e5588ffe6439deec2458a4f4f61d4f60b321c7d
DenisFovas <fovasdenis@gmail.com>
2017-10-19 09:56:07 +0200

    Proba pentru Windows env

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/article',
+    // database: 'mongodb://localhost:27017/article',
+    database: 'mongodb://denis:MONS3R@ds123695.mlab.com:23695/personal-blog',
     secret: 'secret',
 }
